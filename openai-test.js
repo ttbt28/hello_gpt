17fa1158b8b816bf0c02d07ae2356092a3c07869
ttbt28 <ttbt2808@gmail.com>
2023-11-16 11:10:23 -0800

    Initial commit
    A simple chat gpt with static system role

---
+++ openai-test.js
@@ -0,0 +1,17 @@
+const OpenAI = require('openai');
+
+const openai = new OpenAI();
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      { role: "user", content: "What are you?"}
+    ],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0].message.content);
+}
+
+main();
--- src/App.js
+++ src/App.js
@@ -1,22 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import OpenAI from "openai";
+
+const openai = new OpenAI({ apiKey: `${process.env.REACT_APP_OPENAI_API_KEY}`, dangerouslyAllowBrowser: true });
+
+const ChatApp = () => {
+  const [userInput, setUserInput] = useState('');
+  const [chatHistory, setChatHistory] = useState([]);
+  const [selectedModel, setSelectedModel] = useState("gpt-3.5-turbo");
+  const [systemRole, setSystemRole] = useState("You are a helpful assistant.");
+
+  const chat = async (chatPrompt, model) => {
+    var message = [
+      { role: "system", content: `${systemRole}` },
+      { role: "user", content: chatPrompt}
+    ]
+    const completion = await openai.chat.completions.create({
+      messages: message,
+      model: model,
+    });
+    console.log(message)
+    return completion.choices[0].message.content;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Call the chat function with the user input
+    const response = await chat(userInput, selectedModel, systemRole);
+
+    // Update the chat history with the user input and response
+    setChatHistory([...chatHistory, { user: userInput, response }]);
+    
+    // Clear the input field
+    setUserInput('');
+  };
+
+  const handleSystemRoleSubmit = (e) => {
+    e.preventDefault();
+    // Set the system role
+    setSystemRole(systemRoleInput);
+    // Clear the system role input field
+    setSystemRoleInput('');
+  };
+  const [systemRoleInput, setSystemRoleInput] = useState('');
+
+  return (
+    <div>
+      <div style={{
+          position: 'sticky',
+          top: 0,
+          padding: '10px',
+          background: '#fff',
+          zIndex: 1000,
+        }}>
+        <p>GPT Role: {systemRole}</p>
+        <form onSubmit={handleSystemRoleSubmit}>
+          <input
+            type="text"
+            value={systemRoleInput}
+            onChange={(e) => setSystemRoleInput(e.target.value)}
+          />
+          <button type="submit">Set System Role</button>
+        </form>
+      </div>
+      <div>
+        {chatHistory.map((entry, index) => (
+          <div key={index}>
+            <strong>User:</strong> {entry.user}
+            <br />
+            <strong>Response:</strong> {entry.response}
+            <hr />
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+        />
+        <select
+          value={selectedModel}
+          onChange={(e) => setSelectedModel(e.target.value)}
+        >
+          <option value="gpt-3.5-turbo">GPT 3.5</option>
+          <option value="gpt-4">GPT 4</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello GPT</h1>
+        <ChatApp />
       </header>
     </div>
   );
